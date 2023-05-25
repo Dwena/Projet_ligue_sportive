e0c395b1543242a6410334f59fe270b97c5de2b4
Lucas MONNET-POUPON <lucas.monnet-poupon@cgi.com>
2023-05-25 12:36:15 +0200

    home + avatar/user/catalogue/orders

--- src/views/Signin.js
+++ src/views/Signin.js
@@ -1,6 +1,25 @@
 import {Link} from "react-router-dom";
 
 export function Signin() {
+
+    let userAdmin = {
+        firstName: 'Jane',
+        lastName: 'Cooper',
+        phone: '+1 (555) 123-4567',
+        email: 'janeCooper@gmail.com',
+        password: '**********',
+        administrator: true,
+    }
+
+    let userClient = {
+        firstName: 'Eliott',
+        lastName: 'Cooper',
+        phone: '+1 (555) 123-4567',
+        email: 'EliotCooper@gmail.com',
+        password: '**********',
+        administrator: false,
+    }
+
     return (
         <>
             <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
@@ -62,7 +81,7 @@ export function Signin() {
                                 type="submit"
                                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                             >
-                                <Link to={'/accueil'}>
+                                <Link to={'/accueil'} state={userAdmin}>
                                     Se connecter
                                 </Link>
                             </button>
