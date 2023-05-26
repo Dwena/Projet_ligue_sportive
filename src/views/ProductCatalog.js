69657a7814262a2d440d37db1d1eeea7c83d654e
simsam2831 <simondaniel2831@gmail.com>
2023-05-26 09:17:19 +0200

    Show product's image

--- src/views/ProductCatalog.js
+++ src/views/ProductCatalog.js
@@ -36,7 +36,7 @@ export default function ProductCatalog() {
                                 <div
                                     className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
                                     <img
-                                        src={product.imageSrc}
+                                        src={product.img}
                                         alt={product.imageAlt}
                                         className="h-full w-full object-cover object-center group-hover:opacity-75"
                                     />
